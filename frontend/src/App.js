e749f8e5df3be31ef47e88fbfad396d1673d908e
maher elsayed <maher.elsayed.dev@gmail.com>
2020-12-01 02:17:30 +0200

    React bootstrap Setup, Header& Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap';
+
+
+
 const App = () => {
   return (
     <>
-      <h1>Hello MERN E-Commerce</h1>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <h1>Hello MERN E-Commerce</h1>
+        </main >
+      </Container>
+
+
+      <Footer />
     </>
   );
 }
