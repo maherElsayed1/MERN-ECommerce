fab919a2281f6368488c9099512f1d797010ad36
maher elsayed <maher.elsayed.dev@gmail.com>
2020-12-05 03:57:30 +0200

    HomeScreen Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 
 
@@ -11,7 +12,7 @@ const App = () => {
       <Header />
       <Container>
         <main className="py-3">
-          <h1>Hello MERN E-Commerce</h1>
+          <HomeScreen />
         </main >
       </Container>
 
